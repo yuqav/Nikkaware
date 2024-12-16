c916d314e9c977795e50bb46228179e0e5dbcdf9
yuqav <tronyax@gmail.com>
2024-12-16 23:34:29 +0300

    Create 105429890049681.lua

---
+++ Functions/105429890049681.lua
@@ -0,0 +1,46 @@
+local NikkaLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/yuqav/Nikkaware/refs/heads/main/gui.lua')))()
+local Window = NikkaLib:MakeWindow({Name = "NikkaWare", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local homeTab = Window:MakeTab({Name = "Home", Icon = "", PremiumOnly = false})
+local mainTab = Window:MakeTab({Name = "Main", Icon = "", PremiumOnly = false})
+local renderTab = Window:MakeTab({Name = "Render", Icon = "", PremiumOnly = false})
+local autoWin = false
+homeTab:AddParagraph("Dung Beetle Simulator","Last update: 16.12.24 \nMade by: yuqav")
+
+function autow()
+    while autoWin do
+        local args = {
+            [1] = "Area_4"
+        }
+        
+        game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.5.1").knit.Services.FightService.RE.StartFight:FireServer(unpack(args))
+        game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.5.1").knit.Services.FightService.RE.FinishFight:FireServer(unpack(args))
+        task.wait()
+    end
+end
+
+mainTab:AddToggle({
+    Name = "Auto Win",
+    Default = false,
+    Callback = function(v)
+        autoWin = v
+        if autoWin then
+            autow()
+        end
+    end
+})
+
+renderTab:AddSlider({
+    Name = "Time",
+    Min = 0,
+    Max = 24,
+    Default = 14,
+    Color = Color3.fromRGB(190, 190, 190),
+    Increment = 1,
+    ValueName = "Value",
+    Callback = function(s)
+        while task.wait() do
+        game.Lighting.ClockTime = s
+        end
+    end    
+})
