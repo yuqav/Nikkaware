526d33d6a6bba92303c87cc7d6b1ebd181588e97
yuqav <tronyax@gmail.com>
2024-12-17 10:55:31 +0300

    Update 105429890049681.lua

--- Functions/105429890049681.lua
+++ Functions/105429890049681.lua
@@ -5,17 +5,18 @@ local homeTab = Window:MakeTab({Name = "Home", Icon = "", PremiumOnly = false})
 local mainTab = Window:MakeTab({Name = "Main", Icon = "", PremiumOnly = false})
 local renderTab = Window:MakeTab({Name = "Render", Icon = "", PremiumOnly = false})
 local autoWin = false
-homeTab:AddParagraph("Dung Beetle Simulator","Last update: 16.12.24 \nMade by: yuqav")
+homeTab:AddParagraph("Dung Beetle Simulator","Last update: 17.12.24 \nMade by: yuqav")
 
 function autow()
     while autoWin do
         local args = {
             [1] = "Area_4"
         }
-        
+        for i=1, 50 do
         game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.5.1").knit.Services.FightService.RE.StartFight:FireServer(unpack(args))
         game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.5.1").knit.Services.FightService.RE.FinishFight:FireServer(unpack(args))
         task.wait()
+        end
     end
 end
 
